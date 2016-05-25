b6fd19e6e87acc566f1852acdc07a30afe131ec2
Ryan Swapp <ryancswapp@gmail.com>
2016-05-24 20:42:54 -0600

    Updated to Elm 0.17

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 require('./index.html');
-require('./styles/style.css');
 
 var Elm = require('./Main');
 
-Elm.embed(Elm.Main, document.getElementById('main'));
+Elm.Main.embed(document.getElementById('main'));
